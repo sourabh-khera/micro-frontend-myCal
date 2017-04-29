d662208452a6b752d8cf9d264715b308a8de782c
sourabh <=>
2017-04-29 16:13:19 +0530

    Conflicts Resolved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login/component';
@@ -7,6 +7,10 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+import Navbar from "./Home/component"
+import "./Home/Style.css"
+import {connect} from "react-redux"
+import {fetchEvents} from "./action"
 import  SignUp from './Signup/component'
 
 import  EventList from './Events/EventList';
@@ -39,25 +43,38 @@ const Home = ({ match }) => (
   </div>
 );
 
-const App = () => (
-    <Router>
-        <div>
-            <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h2>Welcome to Intellimeet</h2>
-            </div>
-            <div className="nav">
-                <ul>
-                    <li><Link to="/">Login</Link></li>
-                    <li><Link to="/signup">SignUp</Link></li>
-                    <li><Link to="/eventlist">LIst</Link></li>
-                </ul>
-            </div>
-            <Route exact path="/" component={Login}/>
-            <Route path="/signup" component={SignUp}/>
-            <Route path="/home" component={Home}/>
-            <Route path="/eventlist" component={EventList}/>
-        </div>
-    </Router>
-)
-export default App
+class App extends Component {
+
+    componentWillMount(){
+        this.props.dispatch(fetchEvents())
+
+    }
+    render() {
+        console.log(this.props)
+        return (
+            <Router>
+                <div>
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Welcome to Intellimeet</h2>
+                    </div>
+                    <div className="nav">
+                        <ul>
+                            <li><Link to="/">Login</Link></li>
+                            <li><Link to="/signup">SignUp</Link></li>
+                            <li><Link to="/eventlist">LIst</Link></li>
+                        </ul>
+                    </div>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/signup" component={SignUp}/>
+                    <Route path="/home" component={Home}/>
+                    <Route path="/eventlist" component={EventList}/>
+                    <Route exact path="/" component={Navbar}/>
+                </div>
+            </Router>
+        )
+    }
+}
+
+const AppContainer=connect(state=>state)(App)
+export default AppContainer
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import App from './App';
 import './index.css';
+import {Provider} from"react-redux"
+import store from "./store"
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(
-    <MuiThemeProvider>
-        <App />
-    </MuiThemeProvider>,
-  document.getElementById('root')
+render(
+  <Provider store={store}>
+      <MuiThemeProvider>
+    <App />
+      </MuiThemeProvider>
+    </Provider>,
+  document.getElementById('app')
 );
